5b5ba784f6d0d76d0489e5ada0d9e6c48eee7fa6
thdefn <thdefn@gmail.com>
2022-10-27 15:32:23 +0900

    init

---
+++ src/main/java/com/haden/prjforstd/PrjforstdApplication.java
@@ -0,0 +1,13 @@
+package com.haden.prjforstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjforstdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjforstdApplication.class, args);
+    }
+
+}
