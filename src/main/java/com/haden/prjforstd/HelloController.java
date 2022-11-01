f2c815c08d09eaa4d5a801d01078646b110ef62c
thdefn <thdefn@gmail.com>
2022-11-02 07:43:17 +0900

    Spring Web MVC

---
+++ src/main/java/com/haden/prjforstd/HelloController.java
@@ -0,0 +1,27 @@
+package com.haden.prjforstd;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/visit")
+public class HelloController {
+    private static long visitCount = 0;
+
+    @GetMapping("/dynamic")
+    public String dynamicEntry(Model model){
+        visitCount++;
+        model.addAttribute("visits",visitCount);
+        return "hello-visit";
+    }
+
+    @GetMapping("/static")
+    public String staticEntry(){
+        return "redirect:/hello.html";
+    }
+
+
+
+}
--- src/main/java/com/haden/prjforstd/tryFinally/TrywithResources.java
+++ src/main/java/com/haden/prjforstd/tryFinally/TrywithResources.java
@@ -69,9 +69,4 @@ public class TrywithResources {
     }
 
 
-
-
-
-
-
 }
