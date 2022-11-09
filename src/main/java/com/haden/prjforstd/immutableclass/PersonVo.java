e2b9a0f8ab73b3b00f0aecd3deae87aba41bd3e4
thdefn <thdefn@gmail.com>
2022-11-09 09:48:54 +0900

    immutable class

---
+++ src/main/java/com/haden/prjforstd/immutableclass/PersonVo.java
@@ -0,0 +1,23 @@
+package com.haden.prjforstd.immutableclass;
+
+import com.haden.prjforstd.comparable.Person;
+import lombok.Getter;
+
+@Getter
+public class PersonVo {
+    // immutable한 VO를 만드는 실제 케이스
+    // get만 있고 set이 없음 생성자는 private
+    // 값이 변하지 않는 객체라면 다음과 같이 불변성을 강제하는 것 또한 방법이다
+    private final String name;
+    private final double height;
+
+    private PersonVo(String name, double height) {
+        this.name = name;
+        this.height = height;
+    }
+    // from이라는 팩토리메서드를 통해 PersonVo를 만드록 더 이상 변경할 수 없게
+
+    public static final PersonVo from(Person p){
+        return new PersonVo(p.getName(), p.getHeight());
+    }
+}
